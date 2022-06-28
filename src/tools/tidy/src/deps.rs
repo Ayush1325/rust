55203070bee6bab4926985dabd98f82472f5f109
Ayush Singh <ayushsingh1325@gmail.com>
2022-06-28 23:08:26 +0530

    Fix Cargo.lock
    I had run cargo update earliar which basically broke tidy, so well,
    rolling that back.
    Also switched to using git versions of r-efi and compiler-builtins
    patches so that other people can also build this.
    
    Signed-off-by: Ayush Singh <ayushsingh1325@gmail.com>

--- src/tools/tidy/src/deps.rs
+++ src/tools/tidy/src/deps.rs
@@ -21,6 +21,7 @@ const LICENSES: &[&str] = &[
     "0BSD OR MIT OR Apache-2.0", // adler license
     "Zlib OR Apache-2.0 OR MIT", // tinyvec
     "MIT OR Zlib OR Apache-2.0", // miniz_oxide
+    "MIT OR Apache-2.0 OR LGPL-2.1-or-later", // r-efi
 ];
 
 /// These are exceptions to Rust's permissive licensing policy, and
@@ -255,6 +256,7 @@ const PERMITTED_DEPENDENCIES: &[&str] = &[
     // this is a false-positive: it's only used by rustfmt, but because it's enabled through a
     // feature, tidy thinks it's used by rustc as well.
     "yansi-term",
+    "r-efi",
 ];
 
 const PERMITTED_CRANELIFT_DEPENDENCIES: &[&str] = &[
